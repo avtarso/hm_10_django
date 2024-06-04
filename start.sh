77fa0743ee5bf89c08f14eb803d050ca54f744a3
Avtarso <t0676352927@gmail.com>
2024-06-04 16:54:53 +0300

    fix for hosting

--- start.sh
+++ start.sh
@@ -5,7 +5,4 @@ cd bestquotes
 python manage.py makemigrations
 python manage.py migrate
 
-# python bestquotes/manage.py collectstatic --noinput
-# kill -9 $(lsof -t -i:8000)
-
 gunicorn bestquotes.wsgi --log-level debug
